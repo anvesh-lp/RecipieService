9fbfa6ce59191121184efa61e95274ceed0aa6c5
anvesh-lp <knanvesh.01@gmail.com>
2023-01-04 17:30:29 -0800

    Initial commit

---
+++ src/main/java/com/anvesh/recipieservice/RecipieServiceApplication.java
@@ -0,0 +1,13 @@
+package com.anvesh.recipieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipieServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/anvesh/recipieservice/models/Recipie.java
@@ -0,0 +1,2 @@
+package com.anvesh.recipieservice.models;public class Recipie {
+}
